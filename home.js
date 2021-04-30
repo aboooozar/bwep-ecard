6d4b43407dcbeb3056a7cb0d8d7f47b42fba30d7
aboooozar <abuzarramli46@gmail.com>
2021-05-01 05:22:30 +0800

    commit home html, css & js

--- home.js
+++ home.js
@@ -1,5 +1,12 @@
-document.getElementById('button').addEventListener('click', function() {
-  document.querySelector('.box').style.display = 'flex';
-}
+var modalBtn = document.querySelector('.modal-btn');
+var modalBg = document.querySelector('.modal-bg');
+var modalClose = document.querySelector('.modal-close');
 
-);
+
+modalBtn.addEventListener('click',function () {
+  modalBg.classList.add('bg-active');
+});
+
+modalClose.addEventListener('click',function () {
+  modalBg.classList.remove('bg-active');
+});
