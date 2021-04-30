e6a69c7e2d93f71cd0a0cb5ded19bd08527b7697
TqampT <82395871+TqampT@users.noreply.github.com>
2021-04-30 09:07:03 +0800

    informal javascript

---
+++ Informal.js
@@ -0,0 +1,15 @@
+//colour change code
+let colors = ['#FFDFDD','#957DAD','#FEC8D8','#B1D4EC','#F9F0C2','#D2F8DF'];
+//get button
+let button = document.getElementById('button');
+//addEventListener
+button.addEventListener('click', function(){
+  //randomizer
+  var randomColor = colors[Math.floor(Math.random() * colors.length)]
+  //get container
+  let container = document.getElementById('card');
+
+  container.style.background = randomColor;
+})
+// colour change code
+const storageInput = document.querySelector();
